0426aa8b99c02eb4c4695a3bcffaba832ca5198a
ash-valley <42672817+ash-valley@users.noreply.github.com>
2022-10-24 10:14:09 +0900

    test: file output　テスト

--- main.tf
+++ main.tf
@@ -3,19 +3,23 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_cloud_scheduler_job" "slack_notify_scheduler" {
-  name      = "slack-notify-daily"
-  project   = var.project
-  schedule  = "0 12 * * *"
-  time_zone = "Asia/Tokyo"
+# resource "google_cloud_scheduler_job" "slack_notify_scheduler" {
+#   name      = "slack-notify-daily"
+#   project   = var.project
+#   schedule  = "0 12 * * *"
+#   time_zone = "Asia/Tokyo"
 
-  pubsub_target {
-    topic_name = google_pubsub_topic.slack_notify.id
-    data       = base64encode("{\"mention\":\"channel\",\"channel\":\"random\"}")
-  }
-}
+#   pubsub_target {
+#     topic_name = google_pubsub_topic.slack_notify.id
+#     data       = base64encode("{\"mention\":\"channel\",\"channel\":\"random\"}")
+#   }
+# }
 
-resource "google_pubsub_topic" "slack_notify" {
-  name    = "slack-notify"
-  project = var.project
+# resource "google_pubsub_topic" "slack_notify" {
+#   name    = "slack-notify"
+#   project = var.project
+# }
+
+output "foo" {
+  value = file(".env")
 }
